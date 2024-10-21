236f4c1d7f5d169b9a0d126c1289d1bd1bad07b6
Revi1337 <david122123@gmail.com>
2024-10-22 02:20:40 +0900

    [새로운 도메인 리팩토링] - 객체에 메시지 보내기

--- src/main/java/cleancode/studycafe/tobe/model/StudyCafePass.java
+++ src/main/java/cleancode/studycafe/tobe/model/StudyCafePass.java
@@ -18,12 +18,13 @@ public class StudyCafePass {
         return new StudyCafePass(passType, duration, price, discountRate);
     }
 
-    public StudyCafePassType getPassType() {
-        return passType;
+    public boolean isSameDurationType(StudyCafeLockerPass lockerPass) {
+        return lockerPass.isSamePassType(this.passType)
+                && lockerPass.isSameDuration(this.duration);
     }
 
-    public int getDuration() {
-        return duration;
+    public boolean isSamePassType(StudyCafePassType passType) {
+        return this.passType == passType;
     }
 
     public int getPrice() {
@@ -45,6 +46,10 @@ public class StudyCafePass {
             return String.format("%s주권 - %d원", duration, price);
         }
         return "";
+    }
+
+    public boolean cannotUserLocker() {
+        return this.passType.isNotLockerType();
     }
 
 }
